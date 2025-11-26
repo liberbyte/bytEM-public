6ca60b383b9d1b4844c7f82c6748e3b8bfcfbcda
ahmadalsharef994 <ahmadalsharef994@gmail.com>
2025-11-26 15:45:44 +0000

    Add script to create Matrix admin users for liberbyte team

---
+++ scripts/create_users_for_liberbyte.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Script: create_users_for_liberbyte.sh
+# Purpose: Create Matrix admin users in bytem-synapse Docker container for liberbyte team
+
+set -e
+
+USERS=(tero ahmad neel yugank)
+PASSWORD="BytEM2025!"
+CONTAINER="bytem-synapse"
+HOMESERVER_YAML="/data/homeserver.yaml"
+MATRIX_URL="http://localhost:8008"
+
+for user in "${USERS[@]}"; do
+    echo "Registering user: $user"
+    sudo docker exec -i "$CONTAINER" register_new_matrix_user \
+        -c "$HOMESERVER_YAML" \
+        --user "$user" \
+        --password "$PASSWORD" \
+        --admin \
+        "$MATRIX_URL"
+done
